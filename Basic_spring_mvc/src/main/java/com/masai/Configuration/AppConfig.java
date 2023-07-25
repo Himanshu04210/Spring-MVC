c6e5f4a71950090c70a6d707c9aeec30a16a49b1
Himanshu04210 <himanshupoly76075@gmail.com>
2023-07-25 20:54:18 +0530

    doing some practical

---
+++ Basic_spring_mvc/src/main/java/com/masai/Configuration/AppConfig.java
@@ -0,0 +1,29 @@
+package com.masai.Configuration;
+
+/*
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = {
+    "com.masai"
+})
+public class AppConfig implements WebMvcConfigurer {
+
+    @Bean
+    public InternalResourceViewResolver resolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setViewClass(JstlView.class);
+        resolver.setPrefix("/WEB-INF/jsp-files/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+}
+
+*/
