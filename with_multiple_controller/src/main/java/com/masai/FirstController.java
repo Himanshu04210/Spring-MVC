3a230d3b229170b9a252a5e5a4a271407d92fced
Himanshu04210 <himanshupoly76075@gmail.com>
2023-07-27 03:30:26 +0530

    tested and working fine now will write actual logic

--- with_multiple_controller/src/main/java/com/masai/FirstController.java
+++ with_multiple_controller/src/main/java/com/masai/FirstController.java
@@ -1,5 +1,14 @@
 package com.masai;
 
-public class FirstController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
-}
+@Controller
+public class FirstController {
+	
+	@GetMapping("/")
+	public String home() {
+		return "index";
+	}
+	
+}	
