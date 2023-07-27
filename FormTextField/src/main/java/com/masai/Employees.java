07aec3d582bef047a4b721ab30fa4280eef0e4ef
Himanshu04210 <himanshupoly76075@gmail.com>
2023-07-28 01:18:12 +0530

    exploring form tag in jsp

---
+++ FormTextField/src/main/java/com/masai/Employees.java
@@ -0,0 +1,31 @@
+package com.masai;
+
+public class Employees {
+	
+	private String name;
+	private String address;
+	private String department;
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getDepartment() {
+		return department;
+	}
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+	
+	
+	
+}
