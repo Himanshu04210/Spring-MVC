fc471aaea98f0c888a39c1cb5237140950c4e405
Himanshu04210 <himanshupoly76075@gmail.com>
2023-07-28 02:41:58 +0530

    working on configuration

--- FormTextField/src/main/java/com/masai/MyController.java
+++ FormTextField/src/main/java/com/masai/MyController.java
@@ -1,6 +1,14 @@
 package com.masai;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class MyController {
+	
+	@GetMapping("/")
+	public String home() {
+		return "index";
+	}
 	
 }
